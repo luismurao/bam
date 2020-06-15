bba4e5dc8e95508befda98c02ee21b9a86769222
luismurao <luismurao@gmail.com>
2020-06-14 19:35:06 -0500

    Create pam2bioindex.R
    function to estimate dynamic biodiversity index

---
+++ R/pam2bioindex.R
@@ -0,0 +1,11 @@
+#' pam2bioindex: PAM to biodiversity index
+#' @description pam2bioindex estimates various biodiversity indices for a
+#' certain PAM.
+#' @importFrom Rdpack reprompt
+#' @details The biodiversty indices can be found in
+#' \insertCite{Soberon2015;textual}{bam}
+#' @references
+#' \insertRef{Soberon2015}{bam}
+#' @export
+
+pam2bioindex <- function() return()
