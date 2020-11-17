e1ce8771ada252fd9086af2a9d5dae6d1444c66a
luismura <luismurao@gmail.com>
2020-11-17 16:41:24 -0600

    addtional parms

--- R/shape2Grid.R
+++ R/shape2Grid.R
@@ -3,6 +3,7 @@
 #' @description shapeToGrid creates a raster grid given a spatial polygon and a grid resolution.
 #' @param shpolygon A SpatialPolygon, SpatialPolygonDataFrame representing the desired shape of the grid.
 #' @param resolution Numeric. Spatial resoltion of the grid.
+#' @param ones Logical. Fill with ones the values of the raster. If not the values will be written as cellID values.
 #' @return Returns a raster object whith the shape of 'shpolygon' of a given resoltion.
 #' @export
 #' @examples
@@ -13,11 +14,14 @@
 #' }
 
 
-shape2Grid <- function(shpolygon,resolution){
+shape2Grid <- function(shpolygon,resolution,ones=TRUE){
   r1 <- raster::raster()
   raster::extent(r1) <- raster::extent(shpolygon)
   raster::res(r1) <- resolution
-  r1[] <- 1
+  if(ones)
+    r1[] <- 1
+  else
+    r1[] <- 1:raster::ncell(r1)
   r1 <- raster::crop(r1,shpolygon)
   r1 <- raster::mask(r1,shpolygon)
   return(r1)
